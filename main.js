f27406c5f1b2a8fde138abc7fe1c4e091bcda173
DavidMagm <david.martinezezguerra@gmail.com>
2023-05-10 20:50:08 -0500

    agragando el menu movil ya greagandole interaccion

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
-const menuDes = document.querySelector('.desktop-menu')
+const menuDes = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const menuMovil = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleMenuDes);
 
 function toggleMenuDes() {
     menuDes.classList.toggle('inactive');
 }
+
+menuBurger.addEventListener('click', toggleMenuMov);
+
+function toggleMenuMov() {
+    menuMovil.classList.toggle('inactive');
+};
